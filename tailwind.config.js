26faedd49bee895ef857a47ea417efad164c74b3
Victor Hugo Serejo Ferreira <49438011+victorhugo06@users.noreply.github.com>
2025-03-12 15:26:26 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}' // Certifique-se de incluir todos os arquivos do React
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
